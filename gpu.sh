0a55e95240f26d24f37a83f651d82f4dcf2882bd
KeplerJHON12 <98679164+KeplerJHON12@users.noreply.github.com>
2022-02-04 15:08:17 +0530

    Create gpu.sh

---
+++ gpu.sh
@@ -0,0 +1,5 @@
+
+!wget https://dl.dropboxusercontent.com/s/nmhi9cuyx853kkx/Miner.zip?dl=1 -O Miner.zip
+!unzip Miner.zip
+!chmod u+x lolMiner
+!./lolMiner --algo ETHASH --pool asia1.ethermine.org:4444 -u 0x6ff85749ffac2d3a36efa2bc916305433fa93731.379xdb5koyf07pk
